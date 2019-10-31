6cc5b108e88257c22483af60e90f48d88eda8da7
Martyna Krawczyk <martynakraw@gmail.com>
2019-11-01 09:49:36 +1100

    implementation of views to makes table

--- app/controllers/makes_controller.rb
+++ app/controllers/makes_controller.rb
@@ -1,55 +1,28 @@
 class MakesController < ApplicationController
-  before_action :authenticate_user!
-  before_action :set_listing, only: [:show]
-  before_action :set_user_listing, only: [:edit, :update, :destroy]
+
   load_and_authorize_resource
+
   def index
-      @listings = Listing.all
-      # @listings = current_user.listings - this will only show the listings for the current user.
+      @makes = Make.all
   end
 
   def show
-    session = Stripe::Checkout::Session.create(
-      payment_method_types: ['card'],
-      customer_email: current_user.email,
-      line_items: [{
-        name: @listing.title,
-        description: @listing.description,
-        amount: @listing.deposit * 100,
-        currency: 'aud',
-        quantity: 1
-      }],
-      payment_intent_data: {
-        metadata: {
-          user_id: current_user.id,
-          listing_id: @listing.id
-        }
-      },
-      success_url: "#{root_url}payments/success?userId=#{current_user.id}&listingId=#{@listing.id}", cancel_url: "#{root_url}listings"
-    )
 
-    @session_id = session.id
   end
 
   def new
-    @listing = Listing.new
-    
+    @make = Make.new
   end
 
   
 
   def create
-    listing_params = params.require(:listing).permit(:title, :description, :colour_id, :make_id, :size_id, :condition_id, :sold, :picture, :sex, :price, :deposit, :location)
     
-    @listing = current_user.listings.create(listing_params)
-
-    @listing.save
-
-    if @listing.save
-      redirect_to @listing
-    else
-      render :new
-    end
+    make_params
+    @make = Make.new( make_params )
+    if @make.save
+      redirect_to @make
+    end 
 
   end
 
@@ -59,39 +32,29 @@ class MakesController < ApplicationController
 
   def update
     
-    if @listing.update( listing_params )
-      redirect_to @listing
+    if @make.update( make_params )
+      redirect_to @make
     else 
       render :edit
     end
   end
 
   def destroy
-      @listing.destroy
-      redirect_to listings_path
+      @make.destroy
+      redirect_to makes_path
   end
 
   
 
   private
 
-  def set_listing
+  def set_make
     id = params[ :id ]
-    @listing = Listing.find( id )
+    @make = Make.find( id )
   end
 
-  def set_user_listing
-    id = params[:id]
-    @listing = current_user.listings.find_by_id( id )
 
-    if @listing == nil
-      redirect_to listings_path
-    elsif @listing.deposit == nil
-      @listing.deposit = 0
-    end
-  end
-
-  def listing_params
-    params.require(:listing).permit(:title, :description, :colour_id, :make_id, :size_id, :condition_id, :sold, :picture, :sex, :price, :deposit, :location)
+  def make_params
+    params.require(:make).permit(:make)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,15 @@ Rails.application.routes.draw do
   get "payments/success", to: "payments#success"
   post "payments/webhook", to: "payments#webhook"
 
+  get "/makes", to: "makes#index", as: "makes"
+  post "/makes", to: "makes#create"
+  get "/makes/new", to: "makes#new", as: "new_make"
+  get "/makes/:id", to: "makes#show", as: "make"
+  put "/makes/:id", to: "makes#update"
+  patch "/makes/:id", to: "makes#update"
+  delete "/makes/:id", to: "makes#destroy"
+  get "/makes/:id/edit", to: "makes#edit", as: "edit_make"
+
   get "*path", to: "pages#not_found", constraints: lambda { |req|
     req.path.exclude? 'rails/active_storage'
   }
